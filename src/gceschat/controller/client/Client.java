089bb5ffac0c943faadaf40ac95ffd70b2297d8f
Bibek Shahi <bewakez95@gmail.com>
2015-10-06 12:01:33 +0545

    Controller Classes Setup

---
+++ src/gceschat/controller/client/Client.java
@@ -0,0 +1,17 @@
+package controller.client;
+
+public class Client {
+
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
